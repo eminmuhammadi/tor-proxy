234bd4dedfb7d50e9c04b7045c0d0734f36ee067
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-12-01 20:38:41 +0100

    New circuit generation updated

--- new_circuit.sh
+++ new_circuit.sh
@@ -3,9 +3,11 @@ TOR_PASSWORD="\"password\""
 
 while :
 do
- # Create a new circuit
- (echo "authenticate $TOR_PASSWORD"; echo signal newnym; echo quit) | nc 127.0.0.1 9051
+    echo "New circuit has been created..."
 
- # Sleep for 13 seconds
- sleep 13
+    # Create a new circuit
+    (echo "authenticate $TOR_PASSWORD"; echo signal newnym; echo quit) | nc 127.0.0.1 9051
+
+    # Sleep for 13 seconds
+    sleep 13
 done
